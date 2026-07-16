9ea33be64d669bf0ceb09dfe4242448aea091eeb
shubham <shubhamkverma18@gmail.com>
2026-07-16 20:17:54 +0530

    background smoothness

--- frontend/src/components/BackgroundLayer.jsx
+++ frontend/src/components/BackgroundLayer.jsx
@@ -1,26 +1,38 @@
+import { useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 
 function BackgroundLayer({ image }) {
+  const [currentImage, setCurrentImage] = useState(image);
+
+  useEffect(() => {
+    if (!image) return;
+
+    const img = new Image();
+
+    img.src = image;
+
+    img.onload = () => {
+      setCurrentImage(image);
+    };
+  }, [image]);
+
   return (
     <div className="absolute inset-0 -z-20 overflow-hidden">
       <AnimatePresence mode="wait">
         <motion.div
-          key={image}
+          key={currentImage}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{
-            duration: 1.2,
+            duration: 1.3,
             ease: "easeInOut",
           }}
           className="absolute inset-0"
           style={{
             backgroundImage: `
-              linear-gradient(
-                rgba(0,0,0,0.35),
-                rgba(0,0,0,0.35)
-              ),
-              url(${image})
+              linear-gradient(rgba(0,0,0,.35), rgba(0,0,0,.35)),
+              url(${currentImage})
             `,
             backgroundSize: "cover",
             backgroundPosition: "center",
